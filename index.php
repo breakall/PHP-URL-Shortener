85d0a0caa81eb4194baa52eb7cc01678cb16acb6
= <=>
2012-02-25 10:09:06 -0500

    added divs to the main page to display old URL and new URL

--- index.php
+++ index.php
@@ -11,13 +11,26 @@
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <script type="text/javascript">
 $(function () {
+	var _shorturl;
 	$('#shortener').submit(function () {
 		$.ajax({data: {longurl: $('#longurl').val()}, url: 'shorten.php', complete: function (XMLHttpRequest, textStatus) {
-			$('#longurl').val(XMLHttpRequest.responseText);
+			//$('#shorturl').text(XMLHttpRequest.responseText);
+			_shorturl = XMLHttpRequest.responseText;
+			$('#origurl').text($('#longurl').val());
+			$('#newurl').text(_shorturl);
+			$('#newurl').attr('href', _shorturl);
+			
 		}});
+		
+		
 		return false;
 	});
+
 });
 </script>
+
+<div id="origurl"></div>
+<a id="newurl"></div>
+
 </body>
 </html>
